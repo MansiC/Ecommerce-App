44789f52a73e3a082404763691623768e8beadf3
Mansi Chougule <mansi_chougule@epam.com>
2022-08-05 20:11:54 +0530

    Initial commit - UI components and page structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Cart from "./Pages/Cart";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Product from "./Pages/Product";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return <Cart />;
 };
 
 export default App;
